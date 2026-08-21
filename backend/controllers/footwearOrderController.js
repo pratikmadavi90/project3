8b5abb302769ee28f871ef98e494cf42a30b4018
Pratik Madavi <pratikmadavi11@gmail.com>
2026-08-21 22:07:23 +0530

    Added footwear orders admin panel

--- backend/controllers/footwearOrderController.js
+++ backend/controllers/footwearOrderController.js
@@ -52,6 +52,27 @@ exports.getUserFootwearOrderDetails = async (req, res) => {
   }
 };
 
+exports.getUserFootwearOrders = async (req, res) => {
+  try {
+    const { email } = req.params;
+
+    const orders = await FootwearOrder.find({
+      email: email,
+    }).sort({ createdAt: -1 });
+
+    res.json({
+      success: true,
+      data: orders,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
 // ==========================
 // GET ALL ORDERS
 // ==========================
@@ -164,3 +185,9 @@ exports.updateFootwearOrderStatus = async (
 
   }
 };
+
+
+console.log(
+  "getUserFootwearOrderDetails =",
+  typeof exports.getUserFootwearOrderDetails
+);
