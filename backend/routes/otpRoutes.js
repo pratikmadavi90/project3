43a315eac92535595f0e21e72afaffef60a6dcdc
Pratik Madavi <pratikmadavi11@gmail.com>
2026-09-04 18:40:57 +0530

    Added OTP login setup

--- backend/routes/otpRoutes.js
+++ backend/routes/otpRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-  sendOtp,
-} = require("../controllers/otpController");
+const { sendOtp } = require("../controllers/otpController");
+
+router.get("/test", (req, res) => {
+  res.json({
+    success: true,
+    message: "OTP Route Working"
+  });
+});
 
 router.post("/send-otp", sendOtp);
 
